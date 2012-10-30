9eeaecad6babc39d55d784e05d779040879045a8
Jonathon Vogel <jonathon.vogel@gmail.com>
2012-10-30 13:09:41 -0400

    Fix the indentation to use spaces instead of tabs to align. Blah blah mixing spaces and tabs, shoot me.

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 function addShims() {
 	//http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 	window.requestAnimationFrame = window.requestAnimationFrame || 
-							 window.webkitRequestAnimationFrame || 
-							 window.mozRequestAnimationFrame    || 
-							 window.oRequestAnimationFrame      || 
-							 window.msRequestAnimationFrame     || 
-							 function(callback, time){
-							 	if (typeof(time) == 'undefined') time = 1000 / 60
-							 	window.setTimeout(callback, time);
-							 };
+	                         window.webkitRequestAnimationFrame || 
+	                         window.mozRequestAnimationFrame    || 
+	                         window.oRequestAnimationFrame      || 
+	                         window.msRequestAnimationFrame     || 
+	                         function(callback, time){
+	                         	if (typeof(time) == 'undefined') time = 1000 / 60
+	                         	window.setTimeout(callback, time);
+	                         };
 }
