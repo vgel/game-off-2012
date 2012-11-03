40a2d83db36facb635501c3c86b1952af98a46b7
Jonathon Vogel <jonathon.vogel@gmail.com>
2012-11-03 17:35:33 -0400

    Thought I was going to use Object.keys, so added a shim. Turned out I didn't need it, but leaving it in since it might be useful later.

--- utils.js
+++ utils.js
@@ -15,13 +15,50 @@
               timeToCall)
             lastTime = currTime + timeToCall
             return id
-        };
+        }
  
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id)
-        };
+        }
 }());
+
+//From http://whattheheadsaid.com/2010/10/a-safer-object-keys-compatibility-implementation, improvements / style tweaks by me
+Object.keys = Object.keys || (function () {
+	//older IE DOM-elements don't have hasOwnProperty.
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+        dontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+ 
+    return function (o) {
+        if (typeof o !== "object" && typeof o !== "function" || o === null)
+            throw new TypeError("Object.keys called on a non-object");
+ 
+        var result = [];
+        for (var name in o) {
+            if (hasOwnProperty.call(o, name))
+                result.push(name);
+        }
+ 
+        if (hasDontEnumBug) {
+            for (var i = 0; i < dontEnumsLength; i++) {
+                if (hasOwnProperty.call(o, dontEnums[i]))
+                    result.push(dontEnums[i]);
+            }
+        }
+ 
+        return result;
+    };
+})();
 
 function pointInRect(x, y, width, height, px, py) {
 	return px >= x && px <= x + width &&
