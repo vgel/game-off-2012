c0110f420f3b34849d06c4c2b9ab1bea88a4d9c9
Jonathon Vogel <jonathon.vogel@gmail.com>
2012-11-03 17:33:49 -0400

    Change this.width/this.height to static variables so they can be used without a tile object (and because I wasn't planning on differently-size tiles, anyway), and change offset funcs to use a generator function to make it shorter/DRYier.

--- tile.js
+++ tile.js
@@ -1,63 +1,49 @@
+var tile_width = 16
+var tile_height = 16
+
 function Tile(level, x, y) {
 	this.level = level
 	this.x = x
 	this.y = y
-	this.width = 16
-	this.height = 16
 }
 
 Tile.prototype.pointIn = function(x, y) {
-	return pointInRect(this.x, this.y, this.width, this.height, x, y)
+	return pointInRect(this.x, this.y, tile_width, tile_height, x, y)
 }
 
-Tile.prototype.clips = function(x, y, width, height) {
-	return rectanglesClip(this.x, this.y, this.width, this.height, x, y, width, height)
+Tile.prototype.clips = function(x, y, tile_width, tile_height) {
+	return rectanglesClip(this.x, this.y, tile_width, tile_height, x, y, tile_width, tile_height)
 }
 
 Tile.prototype.drawShadow = function(drawctx) {
 	drawctx.fillStyle = "rgba(10, 10, 10, 0.4)"
 	if (this.bottom() == null && this.bottomleft() != null)
-		drawctx.fillRect(this.x - 4, this.y - 4, this.width, this.width + 4)
+		drawctx.fillRect(this.x - 4, this.y - 4, tile_width, tile_width + 4)
 	else
-		drawctx.fillRect(this.x - 4, this.y - 4, this.width, this.width)
+		drawctx.fillRect(this.x - 4, this.y - 4, tile_width, tile_width)
 }
 
 Tile.prototype.draw = function(drawctx, basex, basey, screenwidth, screenheight) {
-	if (this.x > basex + screenwidth || this.x + this.width < 0 || basex || this.y > basey + screenheight || this.y + this.height < basey)
+	if (this.x > basex + screenwidth || this.x + tile_width < 0 || basex || this.y > basey + screenheight || this.y + tile_height < basey)
 		return
 	this.drawShadow(drawctx)
 	drawctx.fillStyle = "rgb(0, 0, 0)"
-	drawctx.fillRect(this.x, this.y, this.width, this.height)
+	drawctx.fillRect(this.x, this.y, tile_width, tile_height)
 }
 
-Tile.prototype.left = function() {
-	return this.level.getTileAt(this.x - this.width, this.y)
+var generateTileOffsetFunc = function(xOffset, yOffset) {
+	if (typeof(xOffset) !== 'number' || typeof(yOffset) !== 'number')
+		throw new TypeError("generateTileOffsetFunc called with non-number(s) " + xOffset + ", " + yOffset)
+	return function() {
+		return this.level.getTileAt(this.x + xOffset, this.y + yOffset)
+	}
 }
 
-Tile.prototype.right = function() {
-	return this.level.getTileAt(this.x + this.width, this.y)
-}
-
-Tile.prototype.top = function() {
-	return this.level.getTileAt(this.x, this.y - this.height)
-}
-
-Tile.prototype.bottom = function() {
-	return this.level.getTileAt(this.x, this.y + this.height)
-}
-
-Tile.prototype.topleft = function() {
-	return this.level.getTileAt(this.x - this.width, this.y - this.height)
-}
-
-Tile.prototype.topright = function() {
-	return this.level.getTileAt(this.x + this.width, this.y - this.height)
-}
-
-Tile.prototype.bottomleft = function() {
-	return this.level.getTileAt(this.x - this.width, this.y + this.height)
-}
-
-Tile.prototype.bottomright = function() {
-	return this.level.getTileAt(this.x + this.width, this.x + this.height)
-}
+Tile.prototype.left        = generateTileOffsetFunc(-tile_width,            0)
+Tile.prototype.right       = generateTileOffsetFunc( tile_width,            0)
+Tile.prototype.right       = generateTileOffsetFunc(          0, -tile_height)
+Tile.prototype.bottom      = generateTileOffsetFunc(          0,  tile_height)
+Tile.prototype.topleft     = generateTileOffsetFunc(-tile_width, -tile_height)
+Tile.prototype.topright    = generateTileOffsetFunc( tile_width, -tile_height)
+Tile.prototype.bottomleft  = generateTileOffsetFunc(-tile_width,  tile_height)
+Tile.prototype.bottomright = generateTileOffsetFunc( tile_width,  tile_height)
